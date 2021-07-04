d7e44888f27a27c581849bcf983cdb20c3418aef
nhquang <quangpersie1@gmail.com>
2021-07-04 12:57:04 -0400

    dashboard page added

---
+++ hra/src/components/home/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sm-page">
+    <div class="sm-page-nav">
+      <div class="row">
+        <div class="col-6 px-2 my-auto">
+          <p>Dashboard</p>
+        </div>
+      </div>
+    </div>
+    <div class="sm-page-body"></div>
+  </div>
+</template>
