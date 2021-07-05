b4fbd221385b469ebb714d9f08d10823429d9637
nhquang <quangpersie1@gmail.com>
2021-07-04 23:22:31 -0400

    edit account page added

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -7,6 +7,35 @@
         </div>
       </div>
     </div>
-    <div class="sm-page-body"></div>
+    <div class="sm-page-body">
+      <div class="col-lg-6 col-xl-4 col-md-6 col-sm-12 p-0">
+        <div class="row sm-page-block mx-2">
+          <div class="title">Account Info</div>
+          <div class="col-12 mb-2 px-2">
+            <label class="req">Username</label>
+            <input type="text" value="nhquang"/>
+          </div>
+             
+          <div class="col-12 mb-2 px-2">
+            <label class="req">Password</label>
+            <input type="password" value="sadasdasdas">        
+          </div>       
+          <div class="col-12 mb-2 px-2">
+            <label class="req">Confirm Password</label>
+            <input type="password" value="sadasdasdas"/>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sm-page-footer">
+      <div class="row">
+        <div class="col-12  my-auto">                  
+          <button class="btn save-btn">
+            <i class="fa fa-check"></i>
+            <span>Save</span>
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
