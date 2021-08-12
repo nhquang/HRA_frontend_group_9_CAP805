96fbaf31052fa4c4ddd9b56dba1d25c683af5b97
nhquang <quangpersie1@gmail.com>
2021-08-12 11:29:02 -0400

    http to https

--- store/store.js
+++ store/store.js
@@ -4,7 +4,7 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        apiUrl: 'http://hrm-backend-group-9.herokuapp.com',
+        apiUrl: 'https://hrm-backend-group-9.herokuapp.com',
         loading: false,
         data: [],
     },
